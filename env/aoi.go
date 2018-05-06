ed2a59d995545a48a173d46d4b45cffc95b42aa8
wrq <pyq225@gmail.com>
2018-05-06 23:45:48 +0800

    update

--- env/aoi.go
+++ env/aoi.go
@@ -1,8 +1,19 @@
 package env
 
+import (
+	"net"
+)
+
 type AOIEntity struct {
+	Index       uint16
+	MapIndex    uint32
+	X           uint16
+	Y           uint16
+	Width       uint16
+	Connections map[int32]net.Conn // client id : conn
 }
 
 func (m Map) GetAOIEntities() []AOIEntity {
+	// TODO 把map object 切成很多个aoi entity
 	return nil
 }
